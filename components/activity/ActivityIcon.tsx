a1d53c0ffa258f3997cd83bbdda7db4ad76cdfdb
Moses Martins <mmoses061@gmail.com>
2026-07-21 11:34:16 +0100

    update the background of the icon

--- components/activity/ActivityIcon.tsx
+++ components/activity/ActivityIcon.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const activityConfig = {
     user_suspended: { bg: "bg-[#CD0F0F1A] text-red-600", src: "/UserSuspendedIcon.svg" },
     offer_pending: { bg: "bg-[#C29A06] text-amber-600", src: "/OfferPendingIcon.svg" },
-    offer_alert: { bg: "bg-orange-50 text-orange-600", src: "/OfferAlertIcon.svg" },
+    offer_alert: { bg: "bg-[#9747FF] text-orange-600", src: "/OfferAlertIcon.svg" },
     offer_approved: { bg: "bg-[#00A95D] text-emerald-600", src: "/OfferApprovedIcon.svg" },
     offer_rejected: { bg: "bg-[#E70606] text-rose-600", src: "/OfferRejectedIcon.svg" },
     offer_suspended: { bg: "bg-[#FFC000] text-gray-600", src: "/OfferSuspendedIcon.svg" },

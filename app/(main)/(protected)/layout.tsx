a35a088c1fe2ce921744f10189121107329e4de0
Moses Martins <mmoses061@gmail.com>
2026-08-26 17:55:37 +0100

    Add protected

--- app/(main)/(protected)/layout.tsx
+++ app/(main)/(protected)/layout.tsx
@@ -1,5 +1,6 @@
+import Protected from "@/components/protected/Protected";
 import { ReactNode } from "react";
 
 export default function ProtectedLayout({ children }: { children: ReactNode }) {
-    return <>{children}</>;
+    return <Protected>{children}</Protected>;
 } 

ad61f9d2b56054e03798170c8513de0176e69e4f
Moses Martins <mmoses061@gmail.com>
2026-09-10 18:12:03 +0100

    add paystack pop up utility

--- components/header/desktop/DesktopMenu.tsx
+++ components/header/desktop/DesktopMenu.tsx
@@ -14,7 +14,8 @@ import { FaList } from "react-icons/fa6";
 import { FiHelpCircle, FiHome, FiLogOut, FiUser } from "react-icons/fi";
 import { HiOutlineEnvelope } from "react-icons/hi2";
 import { IoNotificationsOutline } from "react-icons/io5";
-import { PiHeart } from "react-icons/pi";
+import { PiCrown, PiHeart } from "react-icons/pi";
+
 
 export default function DesktopMenu() {
   const [isOpenDropdown, setIsOpenDropdown] = useState(false);
@@ -145,6 +146,11 @@ export default function DesktopMenu() {
                       icon: <HiOutlineEnvelope />,
                       href: "/message",
                       badge: messageCount,
+                    },
+                    {
+                      label: "Premium User",
+                      icon: <PiCrown />,
+                      href: "/premium-user",
                     },
                     {
                       label: "Help & Support",
